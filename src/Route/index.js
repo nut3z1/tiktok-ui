a5297b69e87676945384d8ce6c818ee7566dc5fd
Tuan <tuankaka109@gmail.com>
2022-10-26 23:56:50 +0700

    Cấu hình Router/Layout

---
+++ src/Route/index.js
@@ -0,0 +1,16 @@
+import Home from '~/page/Home';
+import Following from '~/page/Following';
+import Profile from '~/page/Profile';
+import Upload from '~/page/Upload';
+import Search from '~/page/Search';
+import { HeaderOnly } from '~/components/Layout';
+// publicRoutes
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+export const privateRoutes = [];
---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,14 @@
+import Header from '~/components/Layout/components/Header';
+import Sidebar from './Sidebar';
+
+export function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
